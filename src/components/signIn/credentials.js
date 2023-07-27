283e8fd0eb0494f56f989e6c68ee834ff1267c76
Taha Habib <80636271+TrojanPunk@users.noreply.github.com>
2023-07-27 15:21:42 +0530

    Update credentials.js

--- src/components/signIn/credentials.js
+++ src/components/signIn/credentials.js
@@ -4,13 +4,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaVDE2tIMOmzrC4-Q00LPP2hydRHaC4ak",
-  authDomain: "da-vinci-code-5bc6b.firebaseapp.com",
-  projectId: "da-vinci-code-5bc6b",
-  storageBucket: "da-vinci-code-5bc6b.appspot.com",
-  messagingSenderId: "439603548198",
-  appId: "1:439603548198:web:1caf8020d4f143e63d16e7",
-  measurementId: "G-BTGN8HHEZ7"
+  ADD_YOUR_CONFIG
 };
 
 const app = initializeApp(firebaseConfig);
